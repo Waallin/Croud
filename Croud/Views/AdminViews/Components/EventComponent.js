98aba3cdc8c423238f298b3233be1b0e80d83d65
Waallin <viktorwallin@hotmail.se>
2023-01-14 22:02:28 +0100

    nudå

--- Croud/Views/AdminViews/Components/EventComponent.js
+++ Croud/Views/AdminViews/Components/EventComponent.js
@@ -2,13 +2,13 @@ import { StyleSheet, Text, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 
-const EventComponent = ({opponent, time, place, day}) => {
+const EventComponent = ({opponent, time, location, day}) => {
   return (
     <View style={styles.container}>
       <View style={styles.text}>
         <Text style={styles.title}>{opponent}</Text>
         <Text style={styles.extraInfo}>{time}</Text>
-        <Text style={styles.extraInfo}>{place}</Text>
+        <Text style={styles.extraInfo}>{location}</Text>
       </View>
       <View style={styles.dateWrapper}>
         <Text style={styles.date}>{day}</Text>
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
 
   dateWrapper: {
-    flex: 1.5,
+    flex: 2,
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
@@ -58,5 +58,4 @@ const styles = StyleSheet.create({
     color: "grey",
     paddingRight: 15,
   },
-
 });
