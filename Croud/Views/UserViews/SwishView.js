2a1d24729c3021ba44d9b4dc5eb8ece5af07fe28
Waallin <viktorwallin@hotmail.se>
2023-04-20 18:01:03 +0200

    swishview ready

--- Croud/Views/UserViews/SwishView.js
+++ Croud/Views/UserViews/SwishView.js
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { globalStyles } from '../../Styles/global'
 import { useNavigation } from "@react-navigation/native";
-
+import { Feather } from '@expo/vector-icons'; 
 const SwishView = ({route}) => {
     const navigate = useNavigation();
     const gameInfo = route.params.game;
@@ -16,11 +16,20 @@ const SwishView = ({route}) => {
         });
     }
   return (
-    <View style={{flex: 1, width: "100%", justifyContent: "center", alignItems: "center"}}>
-      <Text>Klicka för att godkänna en swish</Text>
-      <TouchableOpacity style={{...globalStyles.primaryGreenBtn, marginTop: 20}} onPress={nav}>
-        <Text style={globalStyles.primaryBtnText}>Godkän</Text>
-      </TouchableOpacity>
+    <View style={{flex: 1, backgroundColor: "white"}}> 
+    <View style={{flex: 1, backgroundColor: globalStyles.primaryGreen, justifyContent: "center", alignItems: "center", borderBottomLeftRadius: 50, borderBottomRightRadius: 50}}>
+    <Feather name="check-circle" size={62} color="white" />
+    <Text style={{fontSize: "32px", color: "white", marginTop: 25}}>Din betalning lyckades!</Text>
+    </View>
+    <View style={{flex: 1, backgroundColor: "white", alignItems: "center", justifyContent: "center"}}>
+      <Text style={{color:"#B7B9BA", fontSize: "20px", fontWeight: "600", marginBottom: 10}}>23 Juli 2021 10:22</Text>
+      <Text style={{color:"#B7B9BA", fontSize: "27px", fontWeight: "700", marginBottom: 10}}>Håsta IBK</Text>
+      <Text style={{color:"#B7B9BA", fontSize: "20px", fontWeight: "600", marginBottom: 10}}>072 211 82 15</Text>
+      <Text style={{color:"#B9B9BA", fontSize: "27px", fontWeight: "600", marginBottom: 50}}>200 kr</Text>
+      <TouchableOpacity style={globalStyles.primaryGreenBtn} onPress={nav}>
+      <Text style={globalStyles.primaryBtnText}>Fortsätt</Text>
+    </TouchableOpacity>
+    </View>
     </View>
   )
 }
